1b5eed6ce47c0cd245cb4eba6df30f9582b977b9
JediLuke <luke.taylor@dubber.net>
2020-03-04 11:43:34 -0600

    Creating a nice looking note

--- lib/gui/components/note.ex
+++ lib/gui/components/note.ex
@@ -4,6 +4,8 @@ defmodule GUI.Component.Note do
   alias Scenic.Graph
   import Scenic.Primitives
 
+  @ibm_plex_mono GUI.Initialize.ibm_plex_mono_hash
+
   def verify(%{
     id: _id,
     top_left_corner: {_x, _y},
@@ -19,9 +21,22 @@ defmodule GUI.Component.Note do
     top_left_corner: {x, y},
     dimensions: {width, height}
   }, _opts) do
+    font_size = 48
     graph =
       Graph.build()
       |> rect({width, height}, translate: {x, y}, fill: :cornflower_blue, stroke: {1, :ghost_white})
+      |> text("New note...",
+         id: :title,
+         font: @ibm_plex_mono,
+         translate: {x+15, y+font_size}, # text draws from bottom-left corner?? :( also, how high is it???
+         font_size: font_size,
+         fill: :black)
+      |> line({{x+15, y+font_size+25}, {x+width-15, y+font_size+25}}, stroke: {3, :black})
+      |> text("This is the note contents.",
+         id: :title,
+         font: @ibm_plex_mono,
+         translate: {x+15, y+font_size+65}, # text draws from bottom-left corner?? :( also, how high is it???
+         fill: :black)
 
     GenServer.call(GUI.Scene.Root, {:register, id})
     {:ok, %{}, push: graph}
