bebf2c81aa5111ec92757f521d75a26acc1ca1e4
JediLuke <luke.taylor@dubber.net>
2020-03-05 16:25:27 -0600

    Dev tools writing & reading a file

--- lib/utilities/dev_tools.ex
+++ lib/utilities/dev_tools.ex
@@ -18,4 +18,16 @@ defmodule DevTools do
   def root_state do
     :sys.get_state(GUI.Scene.Root)
   end
+
+  def file_write do
+    r = File.cwd!
+    IO.inspect r
+
+    {:ok, file} = File.open("./data/franklin.data", [:write])
+    IO.binwrite(file, "world")
+    File.close(file)
+
+    {:ok, file_contents} = File.read("./data/franklin.data")
+    file_contents
+  end
 end
