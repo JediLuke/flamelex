bb67aa2f4df60e8037bb208e52b4216a86bf572f
JediLuke <luke.taylor@dubber.net>
2020-03-09 13:42:54 -0500

    Use macros to tidy up root reducer

---
+++ lib/gui/reducers/root/root_command_mode_reducer.ex
@@ -0,0 +1,50 @@
+defmodule GUI.Reducer.Root.Mode.Command do
+  @moduledoc """
+  Contains module attribute definitions of all the Scenic input events.
+  """
+
+  @doc false
+  defmacro __using__(_opts) do
+    quote do
+
+      def process({%{command_buffer: %{visible?: false}} = state, _graph}, 'SHOW_COMMAND_BUFFER') do
+        {:command_buffer, pid} = state.component_ref |> hd #TODO, eventually we'll have more componenst
+        new_command_buffer_map =
+          state.command_buffer
+          |> Map.replace!(:visible?, true)
+
+        GenServer.cast(pid, {:action, 'SHOW_COMMAND_BUFFER'})
+        %{state|command_buffer: new_command_buffer_map}
+      end
+
+      def process({%{command_buffer: %{visible?: true}} = state, _graph}, 'CLEAR_AND_CLOSE_COMMAND_BUFFER') do
+        new_command_buffer_map =
+          state.command_buffer |> Map.replace!(:visible?, false)
+
+        {:command_buffer, pid} = state.component_ref |> hd #TODO, eventually we'll have more componenst
+        GenServer.cast(pid, {:action, 'CLEAR_BUFFER_TEXT'})
+        GenServer.cast(pid, {:action, 'HIDE_COMMAND_BUFFER'})
+
+        %{state|command_buffer: new_command_buffer_map}
+      end
+
+      def process({%{command_buffer: %{visible?: true}} = state, _graph}, 'COMMAND_BUFFER_BACKSPACE' = action) do
+        {:command_buffer, pid} = state.component_ref |> hd #TODO, eventually we'll have more componenst
+        GenServer.cast(pid, {:action, action})
+        state
+      end
+
+      def process({%{command_buffer: %{visible?: true}} = state, _graph}, {'COMMAND_BUFFER_INPUT', _input} = action) do
+        {:command_buffer, pid} = state.component_ref |> hd #TODO, eventually we'll have more componenst
+        GenServer.cast(pid, {:action, action})
+        state
+      end
+
+      def process({%{command_buffer: %{visible?: true}} = state, _graph}, 'PROCESS_COMMAND_BUFFER_TEXT_AS_COMMAND' = action) do
+        {:command_buffer, pid} = state.component_ref |> hd #TODO, eventually we'll have more componenst
+        GenServer.cast(pid, {:action, action})
+        state
+      end
+    end
+  end
+end
--- lib/gui/reducers/root/root_reducer.ex
+++ lib/gui/reducers/root/root_reducer.ex
@@ -5,44 +5,7 @@ defmodule GUI.RootReducer do
   require Logger
   import Utilities.ComponentUtils
 
-  def process({%{command_buffer: %{visible?: false}} = state, _graph}, 'SHOW_COMMAND_BUFFER') do
-    {:command_buffer, pid} = state.component_ref |> hd #TODO, eventually we'll have more componenst
-    new_command_buffer_map =
-      state.command_buffer
-      |> Map.replace!(:visible?, true)
-
-    GenServer.cast(pid, {:action, 'SHOW_COMMAND_BUFFER'})
-    %{state|command_buffer: new_command_buffer_map}
-  end
-
-  def process({%{command_buffer: %{visible?: true}} = state, _graph}, 'CLEAR_AND_CLOSE_COMMAND_BUFFER') do
-    new_command_buffer_map =
-      state.command_buffer |> Map.replace!(:visible?, false)
-
-    {:command_buffer, pid} = state.component_ref |> hd #TODO, eventually we'll have more componenst
-    GenServer.cast(pid, {:action, 'CLEAR_BUFFER_TEXT'})
-    GenServer.cast(pid, {:action, 'HIDE_COMMAND_BUFFER'})
-
-    %{state|command_buffer: new_command_buffer_map}
-  end
-
-  def process({%{command_buffer: %{visible?: true}} = state, _graph}, 'COMMAND_BUFFER_BACKSPACE' = action) do
-    {:command_buffer, pid} = state.component_ref |> hd #TODO, eventually we'll have more componenst
-    GenServer.cast(pid, {:action, action})
-    state
-  end
-
-  def process({%{command_buffer: %{visible?: true}} = state, _graph}, {'COMMAND_BUFFER_INPUT', _input} = action) do
-    {:command_buffer, pid} = state.component_ref |> hd #TODO, eventually we'll have more componenst
-    GenServer.cast(pid, {:action, action})
-    state
-  end
-
-  def process({%{command_buffer: %{visible?: true}} = state, _graph}, 'PROCESS_COMMAND_BUFFER_TEXT_AS_COMMAND' = action) do
-    {:command_buffer, pid} = state.component_ref |> hd #TODO, eventually we'll have more componenst
-    GenServer.cast(pid, {:action, action})
-    state
-  end
+  use GUI.Reducer.Root.Mode.Command
 
   def process({%{viewport: %{width: w}} = state, graph}, {'NEW_NOTE_COMMAND', contents, buffer_pid: buf_pid}) do
     width  = w / 3
