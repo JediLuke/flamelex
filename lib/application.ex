cd15e1f900f79d764860ada9f424bd8804761d89
JediLuke <luke.taylor@dubber.net>
2020-10-17 18:10:42 -0500

    Complicate the boot process

--- lib/application.ex
+++ lib/application.ex
@@ -3,18 +3,39 @@ defmodule Flamelex.Application do
 
   use Application
 
+
   def start(_type, _args) do
 
     IO.puts "#{__MODULE__} initializing..."
 
-    children = [
-      Flamelex.GUI.TopLevelSupervisor,
+    start_gui? = true
+
+    children =
+        if start_gui? do
+          boot_gui_process_tree() ++ boot_regular_applications()
+        else
+          boot_regular_applications()
+        end
+
+    opts = [strategy: :one_for_one, name: Flamelex.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+
+
+
+
+  defp boot_gui_process_tree do
+    [
+      Flamelex.GUI.TopLevelSupervisor
+    ]
+  end
+
+  defp boot_regular_applications do
+    [
       Flamelex.OmegaMaster,
       Flamelex.Buffer.TopLevelSupervisor,
       # Flamelex.Agent.TopLevelSupervisor, #TODO this is just commented out to stop spamming the log with reminders atm
     ]
-
-    opts = [strategy: :one_for_one, name: Flamelex.Supervisor]
-    Supervisor.start_link(children, opts)
   end
 end
