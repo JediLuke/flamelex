1f1e172959feca3ff615a9af73e4359f0db55f6e
JediLuke <luke.taylor@dubber.net>
2020-10-18 15:38:09 -0500

    archive some BS probably

--- lib/agents/reminder_agent.ex
+++ lib/agents/reminder_agent.ex
@@ -124,3 +124,72 @@ defmodule Flamelex.Agent.Reminders do
     Utilities.Data.replace_tidbit(r, ackd_reminder)
   end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+  # defp process_reminders([], state), do: state
+  # defp process_reminders([{_key, data} = r | rest], state) do
+  #   case reminder_already_pending?(r, state) do
+  #     true ->
+  #       # Logger.info "Reminder was already pending. #{inspect r}"
+  #       process_reminders(rest, state)
+  #     false ->
+  #       # we found a new reminder...
+  #       case data["remind_me_datetime"] do
+  #         nil ->
+  #           Logger.error "Found a reminder #{inspect r} that didn't have a reminder time."
+  #           state = set_up_reminder(r, state, @default_reminder_time_in_minutes)
+  #           process_reminders(rest, state)
+  #         _remind_me_datetime ->
+  #           Logger.info "Found a new reminder! Setting up a reminder... #{inspect r}"
+  #           state = set_up_reminder(r, state)
+  #           process_reminders(rest, state)
+  #       end
+  #   end
+  # end
+
+  # defp reminder_already_pending?(r, state) when is_list(state) do
+  #   state |> Enum.member?(r)
+  # end
+
+  # defp set_up_reminder({_key, _data} = r, state, time_in_minutes) do
+  #   Process.send_after(self(), {:reminder!, r}, time_in_minutes * (60 * 1000))
+  #   state ++ [r]
+  # end
+  # defp set_up_reminder({_key, data} = r, state) do
+  #   now_utc = DateTime.utc_now()
+  #   remind_me_datetime = data["remind_me_datetime"]
+  #   {:ok, remind_me_utc, 0} = remind_me_datetime |> DateTime.from_iso8601()
+  #   case DateTime.compare(remind_me_utc, now_utc) do
+  #     future when future in [:gt] ->
+  #       notify_delay_ms = DateTime.diff(remind_me_utc, DateTime.utc_now()) * 1000 |> IO.inspect
+  #       Process.send_after(self(), {:reminder!, r}, notify_delay_ms)
+  #       state ++ [r]
+  #     past_or_present when past_or_present in [:lt, :eq] ->
+  #       Logger.warn "This reminder is in the past! #{inspect r}"
+  #       Process.send_after(self(), {:reminder!, r}, @default_reminder_time_in_minutes * (60 * 1000))
+  #       state ++ [r]
+  #   end
+  # end
+
+  # defp ack_reminder_in_user_data_file(r) do
+  #   ackd_reminder = r |> TidBit.ack_reminder()
+  #   Utilities.Data.replace_tidbit(r, ackd_reminder)
+  # end
+
+  # add a new buffer to the state's buffer_list
+  # defp add_buffer(state, buf, f) do
+  #   buf_frame = {buf: buf, frame: f}
+
+  #   state
+  #   |> Map.update!(:buffers, fn buf_list -> buf_list ++ [buf_frame] end)
+  # end
