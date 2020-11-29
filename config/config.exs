b03a2269d6e137aa38a718ab777eff0b392493e1
JediLuke <luke.taylor@dubber.net>
2020-11-29 16:43:17 -0600

    Set test logging level to :warn

--- config/config.exs
+++ config/config.exs
@@ -3,3 +3,8 @@ import Config
 config :elixir,
   # https://hexdocs.pm/elixir/DateTime.html#module-time-zone-database
   :time_zone_database, Tzdata.TimeZoneDatabase
+
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
