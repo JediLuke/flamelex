dd70213b852b590c08993ac0696fb901009fde79
JediLuke <luke.taylor@dubber.net>
2021-04-06 01:54:57 -0500

    remove superfluous newline in debug logs

--- config/config.exs
+++ config/config.exs
@@ -1,5 +1,6 @@
 import Config
 
+
 config :elixir,
   # https://hexdocs.pm/elixir/DateTime.html#module-time-zone-database
   :time_zone_database, Tzdata.TimeZoneDatabase
@@ -7,6 +8,12 @@ config :elixir,
 
 config :flamelex,
   :key_mapping, Flamelex.API.KeyMappings.VimClone
+
+
+# remove superfluous newline characters from logs
+# see: https://elixirforum.com/t/why-does-logger-output-in-iex-have-to-have-an-empty-line-after-every-line-logged/21822/4
+config :logger,
+  :console, format: "$time $metadata[$level] $levelpad$message\n"
 
 
 # Import environment specific config. This must remain at the bottom
