06c78d76a967bb1612f76a55c003680a9f8f57f3
JediLuke <luke.taylor@dubber.net>
2021-11-26 10:11:17 -0600

    Added some of our old friends back to the header section of a tidbit/hypercard

--- lib/flamelex/gui/components/memex/sub_components/hypercard/hypercard.ex
+++ lib/flamelex/gui/components/memex/sub_components/hypercard/hypercard.ex
@@ -6,10 +6,26 @@ defmodule Flamelex.GUI.Component.Memex.HyperCard do
                                         HyperCardDateline}
     alias Flamelex.GUI.Component.Memex.HyperCard.{TagsBox, ToolBox, Body}
 
+    
+    #TODO
+    # - swap title & tabs sections
+    # - work on title component that actually works how we want
+    #       - scissored
+    #       - scrollable (I guess only up/down, not side-side) - also show ... if we overflow our area
+    #       - takes up 2 lines if necessary, just 1 if not
+    # - work on body component displaying how we actually want it to work
+    #       - wraps at correct width
+    #       - renders infinitely long
+    #       - only works for pure text, shows "NOT AVAILABLE" or whatever otherwise (centered ;)
+    # - work on render nice tags & datetime sections
+    # - work on taking us into edit more for a tidbit
 
-    @default_height 300 # the minimum height that any HyperCard will be
+
+    # @default_height 300 # the minimum height that any HyperCard will be
     @inner_margin 15    # How much margin we put in between the border of the card, and text we want to render
     @font_size 24       # How big the standard font is (headers get scaled to this)
+
+    @header_height 200
 
     @buffer_margin 50
     @titlebar_width 0.85
@@ -17,12 +33,15 @@ defmodule Flamelex.GUI.Component.Memex.HyperCard do
 
     @min_body_height 150    # Smallest height for any body, even 1 line of text
 
-    @margins %{
+    @margin %{
         left: 15,
         top: 15,
         right: 15,
         bottom: 15
     }
+
+    @dateline_height 40 #TODO  dont use hard-coded height for dateline
+    @date_margin  10 #TODO dont do this either
 
     @left_margin 15     # How much left margin we leave in the HyperCard
 
@@ -69,20 +88,6 @@ defmodule Flamelex.GUI.Component.Memex.HyperCard do
     end
 
 
-
-    #TODO
-    # - swap title & tabs sections
-    # - work on title component that actually works how we want
-    #       - scissored
-    #       - scrollable (I guess only up/down, not side-side) - also show ... if we overflow our area
-    #       - takes up 2 lines if necessary, just 1 if not
-    # - work on body component displaying how we actually want it to work
-    #       - wraps at correct width
-    #       - renders infinitely long
-    #       - only works for pure text, shows "NOT AVAILABLE" or whatever otherwise (centered ;)
-    # - work on render nice tags & datetime sections
-    # - work on taking us into edit more for a tidbit
-
     def render(%{frame: %{dimensions: %{width: width, height: :flex}} = frame, tidbit: %Memex.TidBit{data: data} = tidbit}) do
 
         #TODO good idea: render each sub-component as a seperate graph,
@@ -95,10 +100,9 @@ defmodule Flamelex.GUI.Component.Memex.HyperCard do
 
 
         # header_height = calc_header_height()
-        header_height = 250 #TODO lets just make this assumption for now
         body_height = calc_wrapped_text_height(%{frame: frame, text: data})
 
-        full_hypercard_height = header_height+body_height
+        full_hypercard_height = @header_height+body_height
 
         # %{x: x, y: y} = frame.top_left
 
@@ -111,18 +115,22 @@ defmodule Flamelex.GUI.Component.Memex.HyperCard do
                     |> Scenic.Primitives.rect({width, full_hypercard_height},
                             id: :hypercard,
                             fill: :rebecca_purple)
-                    |> Scenic.Primitives.rect({width, header_height},
+                    # render just the background for the header section
+                    |> Scenic.Primitives.rect({width, @header_height},
                             id: :hypercard,
                             fill: :deep_sky_blue)
                     |> render_title(%{
                             title: tidbit.title,
                             hypercard_frame: frame })
-                    #Hypercard Dateline
-                    #TagsBox
-                    #ToolBox
+                    |> render_dateline(%{
+                            hypercard_frame: frame })
+                    |> render_tagsbox(%{
+                            hypercard_frame: frame })
+                    # |> render_toolbox(%{
+                    #     hypercard_frame: frame })
                     |> render_body(%{
                             width: width,
-                            header_height: header_height,
+                            header_height: @header_height,
                             data: data })
                 end, [
                     #NOTE: We will scroll this pane around later on, and need to
@@ -137,7 +145,7 @@ defmodule Flamelex.GUI.Component.Memex.HyperCard do
     end
 
     def render_body(graph, %{header_height: header_height, data: text, width: width}) when is_bitstring(text) do
-        textbox_width = width-@margins.left-@margins.right
+        textbox_width = width-@margin.left-@margin.right
         {:ok, metrics} = TruetypeMetrics.load("./assets/fonts/IBMPlexMono-Regular.ttf") #TODO put this in the %Scene{} maybe?
         wrapped_text = FontMetrics.wrap(text, textbox_width, @font_size, metrics)
 
@@ -146,7 +154,7 @@ defmodule Flamelex.GUI.Component.Memex.HyperCard do
             font: :ibm_plex_mono,
             font_size: @font_size,
             fill: :black,
-            translate: {@margins.left, @margins.top+header_height+@font_size}) #TODO this should actually be, one line height
+            translate: {@margin.left, @margin.top+header_height+@font_size}) #TODO this should actually be, one line height
     end
 
     def render_body(graph, %{header_height: header_height, data: _data, width: width}) do
@@ -155,7 +163,7 @@ defmodule Flamelex.GUI.Component.Memex.HyperCard do
             font: :ibm_plex_mono,
             font_size: @font_size,
             fill: :black,
-            translate: {@margins.left, @margins.top+header_height+@font_size}) #TODO this should actually be, one line height
+            translate: {@margin.left, @margin.top+header_height+@font_size}) #TODO this should actually be, one line height
     end
 
     def render_title(graph, %{hypercard_frame: hyper_frame, title: title}) when is_bitstring(title) do
@@ -180,6 +188,28 @@ defmodule Flamelex.GUI.Component.Memex.HyperCard do
          end,
          id: {:hypercard, :title, title}, #TODO how do we register components/primitives
          translate: {@inner_margin, @inner_margin})
+    end
+
+    def render_dateline(graph, %{hypercard_frame: hyper_frame}) do
+        dateline_area = {0.4*(hyper_frame.dimensions.width-(2*@buffer_margin)), @dateline_height}
+        graph
+        |> Scenic.Primitives.rect(dateline_area,
+                fill: :grey,
+                translate: {
+                    @margin.left,
+                    @margin.top+@title_height+@date_margin})
+    end
+
+    def render_tagsbox(graph, %{hypercard_frame: hyper_frame}) do
+        #NOTE Same area as the title
+        tagsbox_area = _title_area = {0.82*(hyper_frame.dimensions.width-(2*@buffer_margin)), @title_height} #TODO dont use hard-coded title_height
+
+        graph
+        |> Scenic.Primitives.rect(tagsbox_area,
+                fill: :red,
+                translate: {
+                    @margin.left,
+                    @margin.top+@title_height+@date_margin+@dateline_height+@date_margin})
     end
 
     #     def render(%{assigns: %{first_render?: true, frame: %Frame{} = frame, tidbit: t}} = scene) do
@@ -427,24 +457,7 @@ defmodule Flamelex.GUI.Component.Memex.HyperCard do
 
 
 
-    def hypercard_scissor(%{top_left: %{x: x, y: y}, dimensions: %{width: w, height: h}}) do
-        {@titlebar_width*(w-(2*@buffer_margin)), @title_height}
-    end
 
-    def hypercard_dateline_frame(%{top_left: %{x: x, y: y}, dimensions: %{width: w, height: h}}) do
-        date_margin = 10
-        dateline_height = 40
-        Frame.new(top_left: {x+@buffer_margin, y+@buffer_margin+@title_height+date_margin},
-                dimensions: {0.4*(w-(2*@buffer_margin)), dateline_height})
-
-    end
-
-    def hypercard_tagsbox_frame(%{top_left: %{x: x, y: y}, dimensions: %{width: w, height: h}}) do
-        date_margin = 10
-        Frame.new(top_left: {x+@buffer_margin, y+@buffer_margin+@title_height*2},
-                dimensions: {@titlebar_width*(w-(2*@buffer_margin)), @title_height}) # same width & height as the title
-
-    end
 
     def hypercard_toolbox_frame(%{top_left: %{x: x, y: y}, dimensions: %{width: w, height: h}}) do
         date_margin = 10
@@ -508,7 +521,7 @@ defmodule Flamelex.GUI.Component.Memex.HyperCard do
     def calc_wrapped_text_height(%{frame: frame, text: unwrapped_text}) when is_bitstring(unwrapped_text) do
 
         width = frame.dimensions.width
-        textbox_width = width-@margins.left-@margins.right
+        textbox_width = width-@margin.left-@margin.right
 
         {:ok, metrics} = TruetypeMetrics.load("./assets/fonts/IBMPlexMono-Regular.ttf")
         wrapped_text = FontMetrics.wrap(unwrapped_text, textbox_width, @font_size, metrics)
@@ -522,7 +535,7 @@ defmodule Flamelex.GUI.Component.Memex.HyperCard do
             |> Scenic.Primitives.text(wrapped_text, font: :ibm_plex_mono, font_size: @font_size)
             |> Scenic.Graph.bounds()
         
-        body_height = (bottom-top)+@margins.top+@margins.bottom
+        body_height = (bottom-top)+@margin.top+@margin.bottom
 
         if body_height <= @min_body_height do
             @min_body_height
