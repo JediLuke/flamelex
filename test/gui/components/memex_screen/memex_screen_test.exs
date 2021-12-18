4b32830eb5633062e459897a76ab2352361bebfe
JediLuke <luke.taylor@dubber.net>
2021-12-18 08:20:11 -0600

    Property test scratch code / infra / at least its nearly in place

---
+++ test/gui/components/memex_screen/memex_screen_test.exs
@@ -0,0 +1,13 @@
+defmodule Flamelex.Test.GUI.Components.MemexScreen do
+
+    use ExUnitProperties
+
+    # property "bin1 <> bin2 always starts with bin1" do
+    #     check all bin1 <- binary(),
+    #                 bin2 <- binary() do
+    #         assert String.starts_with?(bin1 <> bin2, bin1)
+    #     end
+    # end
+
+end
+  
