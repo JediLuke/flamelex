95fe0d367710c7e14df80661bd00eeffccb3efbd
Luke T <luke.taylor@dubber.net>
2022-12-30 07:59:30 -0600

    rename to file explorer & code for building nav tree

--- lib/gui/components/file_explorer.ex
+++ lib/gui/components/file_explorer.ex
@@ -1,4 +1,4 @@
-defmodule Flamelex.GUI.Component.FileTree do
+defmodule Flamelex.GUI.Component.FileExplorer do
    use Scenic.Component
    alias ScenicWidgets.Core.Structs.Frame
    require Logger
@@ -10,6 +10,9 @@ defmodule Flamelex.GUI.Component.FileTree do
    end
 
    def init(scene, args, opts) do
+
+      #TODO consider pulling the initial state from RadixState so that
+      # if it breaks, it automatically updates...
 
       init_graph =
          render(args.frame, args.state)
@@ -62,14 +65,43 @@ defmodule Flamelex.GUI.Component.FileTree do
    end
 
    def calc_side_nav_state(%{open_proj: proj_dir}) when is_bitstring(proj_dir) do
+      if not File.dir?(proj_dir) do
+         raise "The path: #{inspect proj_dir} is not a directory."
+      end
+
+      #TODO order the items here? Alphabetically? Put directorys first?
+      top_lvl_tree_items = File.ls!(proj_dir)
+      
       %{
-         nav_tree: construct_nav_tree(proj_dir)
+         nav_tree: construct_nav_tree(top_lvl_tree_items, [])
       }
    end
 
-   def construct_nav_tree(dir) do
-      files = File.ls!(dir)
-      files
+   def construct_nav_tree([], result) do
+      #TODO move directory's to the top??
+      result # base case
+   end
+
+   def construct_nav_tree([item|rest], result) do
+      if File.dir?(item) do
+         sub_items = File.ls!(item)
+         new_result = result ++ [%{
+            item: {:node, item, construct_nav_tree(sub_items, [])}
+         }]
+         
+         #TODO for each of the sub menu items, need to append the current directory
+
+         construct_nav_tree(rest, new_result)
+      else
+         #TODO need to a) get the full filepath here somehow
+         
+         new_result = result ++ [%{
+            item: {:leaf, item},
+            func: fn -> Flamelex.API.Buffer.open(item) end
+         }]
+
+         construct_nav_tree(rest, new_result)
+      end
    end
 
  end
