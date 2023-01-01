abaccff17281a5feb16b693eb2b4ddb42c40e3a9
Luke T <luke.taylor@dubber.net>
2022-12-31 23:49:50 -0600

    Allow switching betwen open projects

--- lib/fluxus/reducers/projects_reducer.ex
+++ lib/fluxus/reducers/projects_reducer.ex
@@ -11,7 +11,7 @@ defmodule Flamelex.Fluxus.Reducers.Projects do
       {:ok, new_radix_state}
    end
 
-   def process(%{projects: %{open_proj: nil}} = radix_state, {:open_project_directory, project_dir}) do
+   def process(%{projects: %{open_proj: _open_proj}} = radix_state, {:open_project_directory, project_dir}) do
       new_radix_state = radix_state
       |> put_in([:projects, :open_proj], project_dir)
 
